c6c9ef665e95af71117bb16bc3796463af76a78a
Aaron Miller <aaron@thedevcompany.com>
2020-02-28 19:49:47 +0100

    update

--- src/lib/gbx/GBXEditor.js
+++ src/lib/gbx/GBXEditor.js
@@ -15,13 +15,28 @@ export default class GBXEditor extends Component {
     this.onChange = this.onChange.bind(this);
     this.onFocus = this.onFocus.bind(this);
     this.onInit = this.onInit.bind(this);
+		this.setConfig = this.setConfig.bind(this);
+
+    const testContent = `<p>Test</p><p>&nbsp;</p><figure class="media"><oembed url="https://youtu.be/bxY4lpc207Q"></oembed></figure>`;
     this.state = {
-      content: `<p>Test</p><p>&nbsp;</p><figure class="media"><oembed url="https://youtu.be/bxY4lpc207Q"></oembed></figure>`
+      content: ''
     };
   }
 
   componentDidMount() {
   }
+
+	setConfig() {
+		const config = {
+			giveboxUpload: {
+				callback: (file) => {
+					console.log('execute callback', file);
+					return 'https://givebox.s3.amazonaws.com/gbx%2Ff666b47e325591a76ff4e6af3bb17c12%2F2019-05-23%2F1xqql7zxxj8bnihnk4rp-jpg%2Foriginal';
+				}
+			}
+		}
+		return config;
+	}
 
   onInit(editor) {
     console.log( 'Editor is ready to use!', editor );
@@ -46,8 +61,9 @@ export default class GBXEditor extends Component {
   render() {
 
     return (
-      <>
+      <div className='ck-content'>
         <CKEditor
+						config={this.setConfig()}
             editor={ ClassicEditor }
             data={this.state.content}
             onInit={this.onInit}
@@ -55,15 +71,13 @@ export default class GBXEditor extends Component {
             onBlur={this.onBlur}
             onFocus={this.onFocus}
         />
-        <EmbedContainer markup={this.state.content}>
-          <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
-        </EmbedContainer>
+        <div dangerouslySetInnerHTML={{ __html: this.state.content }} />
         {/*
         <figure className='media'>
           <oembed url="https://youtu.be/T7ep3FBF3Is"></oembed>
         </figure>
         */}
-      </>
+      </div>
     )
   }
 }
