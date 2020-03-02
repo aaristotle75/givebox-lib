1ad188e809fe99343fd5e9757679d359b0f49e87
Aaron Miller <aaron@thedevcompany.com>
2020-03-02 19:08:59 +0100

    gbxeditor using ckeditor

--- src/lib/gbx/GBXEditor.js
+++ src/lib/gbx/GBXEditor.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import {
-  util
-} from '../../';
-import '../styles/quill.scss';
+  util,
+	Loader,
+	Alert
+} from '../';
 import CKEditor from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import EmbedContainer from 'react-oembed-container';
+import { handleFile } from './images/ImageHandling';
 
 export default class GBXEditor extends Component {
 
@@ -19,19 +20,42 @@ export default class GBXEditor extends Component {
 
     const testContent = `<p>Test</p><p>&nbsp;</p><figure class="media"><oembed url="https://youtu.be/bxY4lpc207Q"></oembed></figure>`;
     this.state = {
-      content: ''
+      content: '',
+			loading: false,
+			error: null
     };
   }
 
   componentDidMount() {
   }
 
+  componentWillUnmount() {
+    if (this.timeout) {
+      clearTimeout(this.timeout);
+      this.timeout = null;
+    }
+  }
+
 	setConfig() {
 		const config = {
 			giveboxUpload: {
 				callback: (file) => {
-					console.log('execute callback', file);
-					return 'https://givebox.s3.amazonaws.com/gbx%2Ff666b47e325591a76ff4e6af3bb17c12%2F2019-05-23%2F1xqql7zxxj8bnihnk4rp-jpg%2Foriginal';
+					this.setState({ loading: true });
+					const p = new Promise((resolve, reject) => {
+						this.url = null;
+						handleFile(file, (url, error) => {
+							this.setState({ loading: false });
+							resolve(url);
+						});
+					})
+					return p;
+				},
+				uploadError: () => {
+					this.setState({ error: 'Error uploading. Please make sure the image you are uploading is a JPEG, PNG or GIF file.' });
+					this.timeout = setTimeout(() => {
+						this.setState({ error: null });
+						this.timeout = null;
+					}, 4000);
 				}
 			}
 		}
@@ -39,29 +63,33 @@ export default class GBXEditor extends Component {
 	}
 
   onInit(editor) {
-    console.log( 'Editor is ready to use!', editor );
+    //console.log( 'Editor is ready to use!', editor );
   }
 
   onBlur(event, editor) {
     const content = editor.getData();
-    console.log( { event, editor, content } );
+    //console.log( { event, editor, content } );
     this.setState({ content});
   }
 
   onChange(event, editor) {
     const content = editor.getData();
-    console.log( { event, editor, content } );
+    //console.log( { event, editor, content } );
     this.setState({ content });
   }
 
   onFocus(event, editor) {
-    console.log('focus', editor);
+    //console.log('focus', editor);
   }
 
   render() {
 
+		console.log('execute loading', this.state.loading);
+
     return (
       <div className='ck-content'>
+				{this.state.loading && <Loader msg='Loading image...' /> }
+				<Alert alert='error' display={this.state.error} msg={this.state.error} />
         <CKEditor
 						config={this.setConfig()}
             editor={ ClassicEditor }
