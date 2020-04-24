a12e6461cce7bbf7a265af6aba61958ab6904fb9
Aaron Miller <aaron@thedevcompany.com>
2020-04-24 09:48:27 +0200

    tabs to spaces

--- src/lib/gbx3/blocks/Form.js
+++ src/lib/gbx3/blocks/Form.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import {
-	util,
-	GBLink,
-	toggleModal
+  util,
+  GBLink,
+  toggleModal
 } from '../../';
 import PaymentForm from '../payment/PaymentForm';
 import { BlockOption } from './Block';
@@ -11,87 +11,87 @@ import Button from './Button';
 
 class Form extends Component {
 
-	constructor(props) {
-		super(props);
-		this.edit = this.edit.bind(this);
-		this.saveButton = this.saveButton.bind(this);
+  constructor(props) {
+    super(props);
+    this.edit = this.edit.bind(this);
+    this.saveButton = this.saveButton.bind(this);
 
-		const button = {...util.getValue(props.globalOptions, 'button', {}), ...util.getValue(props.options, 'button', {}) };
-		this.state = {
-			button,
-			defaultButton: { ...button },
-			edit: false
-		};
-	}
+    const button = {...util.getValue(props.globalOptions, 'button', {}), ...util.getValue(props.options, 'button', {}) };
+    this.state = {
+      button,
+      defaultButton: { ...button },
+      edit: false
+    };
+  }
 
-	componentDidMount() {
-	}
+  componentDidMount() {
+  }
 
-	saveButton() {
-		const form = document.getElementById(`gbxForm-form-saveButton`);
-		if (form) form.click();
-	}
+  saveButton() {
+    const form = document.getElementById(`gbxForm-form-saveButton`);
+    if (form) form.click();
+  }
 
-	edit() {
-		//this.props.toggleModal(this.props.modalID, true);
-		this.setState({ edit: true });
-	}
+  edit() {
+    //this.props.toggleModal(this.props.modalID, true);
+    this.setState({ edit: true });
+  }
 
-	remove() {
-		console.log('execute remove');
-	}
+  remove() {
+    console.log('execute remove');
+  }
 
-	render() {
+  render() {
 
-		const {
-			article,
-			noRemove,
-			primaryColor
-		} = this.props;
+    const {
+      article,
+      noRemove,
+      primaryColor
+    } = this.props;
 
-		const {
-			edit,
-			button
-		} = this.state;
+    const {
+      edit,
+      button
+    } = this.state;
 
-		return (
-			<div className='block'>
-				<BlockOption
-					edit={edit}
-					noRemove={noRemove}
-					editOnClick={this.edit}
-					removeOnClick={this.remove}
-				/>
-				<PaymentForm
-					primaryColor={primaryColor}
-					article={article}
-					phone={{ enabled: true, required: false }}
-					address={{ enabled: true, required: false }}
-					work={{ enabled: true, required: false }}
-					custom={{ enabled: true, required: false, placeholder: 'My custom note placeholder' }}
-					editable={this.props.editable}
-				/>
-				<div className='button-group'>
-					<Button
-						onClick={this.saveButton}
-						button={button}
-					/>
-					<GBLink allowCustom={true} onClick={() => console.log('onclick callback')}>No, thanks</GBLink>
-				</div>
-			</div>
-		)
-	}
+    return (
+      <div className='block'>
+        <BlockOption
+          edit={edit}
+          noRemove={noRemove}
+          editOnClick={this.edit}
+          removeOnClick={this.remove}
+        />
+        <PaymentForm
+          primaryColor={primaryColor}
+          article={article}
+          phone={{ enabled: true, required: false }}
+          address={{ enabled: true, required: false }}
+          work={{ enabled: true, required: false }}
+          custom={{ enabled: true, required: false, placeholder: 'My custom note placeholder' }}
+          editable={this.props.editable}
+        />
+        <div className='button-group'>
+          <Button
+            onClick={this.saveButton}
+            button={button}
+          />
+          <GBLink allowCustom={true} onClick={() => console.log('onclick callback')}>No, thanks</GBLink>
+        </div>
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state, props) {
 
-	const modalID = `textBlock-${props.name}`;
+  const modalID = `textBlock-${props.name}`;
 
-	return {
-		modalID
-	}
+  return {
+    modalID
+  }
 }
 
 export default connect(mapStateToProps, {
-	toggleModal
+  toggleModal
 })(Form);
