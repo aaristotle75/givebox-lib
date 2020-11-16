e05d274af35b77081d35189c365817c32ca6ae69
Aaron Miller <aaron@thedevcompany.com>
2020-11-16 08:16:48 +0100

    fix bug with updating title

--- src/lib/gbx3/blocks/Text.js
+++ src/lib/gbx3/blocks/Text.js
@@ -84,7 +84,7 @@ class Text extends Component {
       const updateMax = util.getValue(block, 'updateMax');
       console.log('execute updateMax', block.field, updateMax);
       if (updateOptions) data[block.field] = updateOptions === 'string' ? util.remove_non_ascii(util.stripHtml(content)) : content;
-      if (updateMax) {
+      if (updateOptions && updateMax) {
         data[block.field] = data[block.field].replace(/\r?\n|\r/g, '').trim().substring(0, updateMax);
         console.log('execute', data[block.field])
       }
