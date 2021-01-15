71c3b05c33a6c0ff4783ce4fd1cf52af61af06d3
Aaron <aaron.miller333@gmail.com>
2021-01-15 08:31:09 +0000

    fix isDebit reversal

--- src/lib/gbx3/redux/gbx3actions.js
+++ src/lib/gbx3/redux/gbx3actions.js
@@ -496,6 +496,7 @@ function calcFee(amount = 0, fees = {}) {
     const paymethod = util.getValue(cart, 'paymethod', {});
     const cardType = util.getValue(cart, 'cardType');
     const isDebit = util.getValue(cart, 'isDebit');
+    const cardLength = +util.getValue(cart, 'cardLength', 0);
 
     let feePrefix = 'fnd';
     switch (paymethod) {
@@ -509,10 +510,12 @@ function calcFee(amount = 0, fees = {}) {
 
           // no default
         }
-        break;
       }
     }
-    if (isDebit && paymethod === 'echeck') {
+
+    if (paymethod === 'echeck' && cardLength < 16) {
+      feePrefix = 'debit';
+    } else if (paymethod === 'echeck' && isDebit) {
       feePrefix = 'debit';
     }
 
