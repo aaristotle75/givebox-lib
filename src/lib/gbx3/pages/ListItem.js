1013e9d87294e2b5d75fc37a880c001b17ad7ed4
Aaron <aaron.miller333@gmail.com>
2021-02-05 19:46:26 +0000

    card updates

--- src/lib/gbx3/pages/ListItem.js
+++ src/lib/gbx3/pages/ListItem.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import * as util from '../../common/utility';
 import GBLink from '../../common/GBLink';
+import Image from '../../common/Image';
 
 class ListItem extends Component {
   constructor(props){
@@ -15,14 +16,20 @@ class ListItem extends Component {
   render() {
 
     const {
-      title
+      title,
+      imageURL
     } = this.props.item;
 
     return (
       <li>
         <div className='cardContainer'>
-          <div className='cardHeader'>
-
+          <div className='cardPhotoContainer'>
+            <div className='cardPhotoImage'>
+              <Image imgID='cardPhoto' url={imageURL} maxWidth='325px' size='medium' alt='Card Photo' />
+            </div>
+          </div>
+          <div className='cardInfoContainer'>
+            <h2>{util.truncate(title, 64)}</h2>
           </div>
         </div>
       </li>
