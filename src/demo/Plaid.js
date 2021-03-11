c20502acfd401b5c1f8b0f03aca98634081644d8
Aaron <aaron.miller333@gmail.com>
2021-03-11 18:07:18 +0000

    plaid tests

--- src/demo/Plaid.js
+++ src/demo/Plaid.js
@@ -5,33 +5,43 @@ import {
   getResource
 } from '../lib/api/helpers';
 import GBLink from '../lib/common/GBLink';
+import * as util from '../lib/common/utility';
+import { PlaidLink } from 'react-plaid-link';
 
 class Plaid extends React.Component {
 
   constructor(props) {
     super(props);
-    this.linkToken = this.linkToken.bind(this);
+    this.getLinkToken = this.getLinkToken.bind(this);
     this.accessToken = this.accessToken.bind(this);
     this.auth = this.auth.bind(this);
     this.identity = this.identity.bind(this);
+    this.delete = this.delete.bind(this);
     this.state = {
+      linkToken: null
     };
   }
 
   componentDidMount() {
   }
 
-  linkToken() {
+  getLinkToken() {
     this.props.sendResource('plaidLink', {
       method: 'POST',
       callback: (res, err) => {
-        console.log('execute plaidLink -> ', res, err);
+        if (!util.isEmpty(res) && !err) {
+          const linkToken = util.getValue(res, 'linkToken');
+          this.setState({ linkToken });
+        }
       }
     })
   }
 
-  accessToken() {
+  accessToken(publicToken) {
     this.props.sendResource('plaidAccess', {
+      data: {
+        publicToken
+      },
       method: 'POST',
       callback: (res, err) => {
         console.log('execute plaidAccess -> ', res, err);
@@ -57,22 +67,44 @@ class Plaid extends React.Component {
     })
   }
 
+  delete() {
+    this.props.sendResource('plaidAccess', {
+      method: 'DELETE',
+      callback: (res, err) => {
+        console.log('execute plaidAccess DELETE -> ', res, err);
+      }
+    })
+  }
+
   render() {
 
     const {
     } = this.props;
 
+    const {
+      linkToken
+    } = this.state;
+
     return (
       <div>
         <h2>Plaid</h2>
-        <GBLink onClick={() => this.linkToken()}>Create Link Token</GBLink>
+        <GBLink onClick={() => this.getLinkToken()}>Create Link Token</GBLink>
         <br /><br />
-        <GBLink onClick={() => this.accessToken()}>Create Access Token</GBLink>
-        <br /><br />
+        { linkToken ?
+          <div>
+            <PlaidLink
+              token={linkToken}
+              onSuccess={this.accessToken}
+            >
+              Connect a bank account
+            </PlaidLink>
+          </div>
+        : null }
         <GBLink onClick={() => this.auth()}>Get AUTH</GBLink>
         <br /><br />
         <GBLink onClick={() => this.identity()}>Get Identity</GBLink>
         <br /><br />
+        <GBLink onClick={() => this.delete()}>Delete Token</GBLink>
       </div>
     )
   }
