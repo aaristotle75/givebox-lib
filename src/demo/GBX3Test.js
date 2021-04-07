f0cf7ac5821a71ed25c8a96206b2d04f59c0d59a
Aaron <aaron.miller333@gmail.com>
2021-04-07 11:00:54 +0000

    fix card title height

--- src/demo/GBX3Test.js
+++ src/demo/GBX3Test.js
@@ -58,7 +58,7 @@ class GBXTest extends Component {
         <div>
           <GBX3
             browse={false}
-            blockType={'article'}
+            blockType={'org'}
             orgID={orgID}
             articleID={articleID}
             saveCallback={this.saveCallback}
