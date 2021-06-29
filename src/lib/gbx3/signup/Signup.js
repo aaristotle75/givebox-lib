b6a2e87ceb0d342f0ffc35940a184a26f1294c3d
Aaron <aaron.miller333@gmail.com>
2021-06-29 18:32:03 +0000

    remove icons from buttons

--- src/lib/gbx3/signup/Signup.js
+++ src/lib/gbx3/signup/Signup.js
@@ -82,10 +82,10 @@ class Signup extends React.Component {
                   </div>
                   <div className='moneyRaisedContainer'>
                     <GBLink style={{ marginRight: 10 }} className='button' onClick={() => this.props.loadOrgSignup({ bookDemo: true })}>
-                      <span className='buttonAlignText'>Book Demo <span className='icon icon-book-open'></span></span>
+                      <span className='buttonAlignText'>Book Demo</span>
                     </GBLink>
                     <GBLink className='button' onClick={() => this.openStep('account')} style={{ marginRight: 10 }}>
-                      <span className='buttonAlignText'>Save Progress <span className='icon icon-save'></span></span>
+                      <span className='buttonAlignText'>Save Progress</span>
                     </GBLink>
                     <div className='tooltip moneyRaised'>
                       <div className='tooltipTop'>
