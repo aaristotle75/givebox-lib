67e8d8aab38a258c8a73d678e01522f560f7abf5
Aaron Miller <aaron.miller333@gmail.com>
2022-02-15 12:41:13 +0100

    update network error style

--- src/lib/common/NetworkError.js
+++ src/lib/common/NetworkError.js
@@ -19,7 +19,7 @@ export default class NetworkError extends React.Component {
 
     return (
       <div className='flexColumn flexCenter'>
-        <h2 className='center' style={{marginTop: '20px'}}>
+        <h2 className='center'>
           <span style={{ fontSize: 30, display: 'block', marginBottom: 5 }} className='icon icon-alert-circle'></span>
           <strong>A Network Error Occurred</strong>
           <span style={{ fontSize: 16, display: 'block' }}>Please check your internet connection.</span>
